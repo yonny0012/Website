e585fcb3cc0bd96f6f5b9b68dd1345c31268b169
Yonny Domingez <yonnydomingez22@gmail.com>
2023-03-08 18:17:23 -0500

    style: :art: Change style navbar
    le cambie el estilo a la barra de navegacion y el fondo de la seccion hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { NavBar } from './Components/NavBar';
+import { Hero } from './Components/Hero';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <div>
+        <Hero />
+      </div>
     </BrowserRouter>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,8 @@ import code from "../Items/img/code-slash.svg";
 import { Link } from "react-router-dom"; 
 export const NavBar = ()=>{
   return (
-    <nav className="navbar navbar-expand-md navbar-light">
+    // Puse el bg-dark para que la varra de navegacion fuera oscura
+    <nav className="navbar navbar-expand-md bg-dark navbar-light">
       <div className="container-fluid">
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-Toggler" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
