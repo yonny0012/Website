39470696bb884a149c584438979f9372654192a3
Yonny Domingez <yonnydomingez22@gmail.com>
2023-03-11 06:57:22 -0500

    fix: :bug: experiment equilibred

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { NavBar } from './Components/NavBar';
 import { Hero } from './Components/Hero';
-import { ProgresBar } from './Components/ProgresBar';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <ProgressBar now={10}/> 
+      <ProgressBar now={10} animated /> 
       <div>
         <Hero />
       </div>
